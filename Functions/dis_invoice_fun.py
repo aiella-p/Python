5fe9e1f1eaf77893488e574e2faa536cd7565cbf
aiella-p <58145537+aiella-p@users.noreply.github.com>
2025-01-05 11:31:20 +0000

    Add files via upload

---
+++ Functions/dis_invoice_fun.py
@@ -0,0 +1,5 @@
+def display_invoice(uname,amount,due_date):
+    print(f" Hello {uname} Your bill of £{amount} is due on: {due_date}")
+
+display_invoice("Vijay", 150.45, "01/01") 
+display_invoice("David", 250.45, "01/04")    
